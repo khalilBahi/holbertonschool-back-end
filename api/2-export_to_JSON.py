e500a580e7493f9f40513bbf187db0dd531e6ed0
khalilBahi <7753@holbertonstudents.com>
2024-04-16 19:17:19 +0200

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -4,9 +4,9 @@ import json
 import requests
 from sys import argv
 
-if __name__ == '__main__':
-    resp_users = requests.get('https://jsonplaceholder.typicode.com/users')
-    resp_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+if __name__ == "__main__":
+    resp_users = requests.get("https://jsonplaceholder.typicode.com/users")
+    resp_todos = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     user_id = argv[1]
 
@@ -14,15 +14,15 @@ if __name__ == '__main__':
     json_list = list()
     small_dic = dict()
     for i in resp_users.json():
-        if i['id'] == int(user_id):
-            user_name = i['username']
+        if i["id"] == int(user_id):
+            user_name = i["username"]
     for i in resp_todos.json():
-        if i['userId'] == int(user_id):
+        if i["userId"] == int(user_id):
             small_dic = {}
-            small_dic["task"] = i['title']
-            small_dic["completed"] = i['completed']
+            small_dic["task"] = i["title"]
+            small_dic["completed"] = i["completed"]
             small_dic["username"] = user_name
             json_list.append(small_dic)
     json_dic[user_id] = json_list
-    with open(f"{user_id}.json", 'w') as j_file:
+    with open(f"{user_id}.json", "w") as j_file:
         json.dump(json_dic, j_file)
